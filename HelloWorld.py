345aa4a00a9b398fb04c3b6d21d4e453d6fc09f5
Jenny Knuth <inkboco@gmail.com>
2015-02-02 12:02:35 -0700

    Hello World

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+print ("Hello world!")
+
+# an alternative version using inputs and variables as shown in lab
+
+y = raw_input ("Whom are you greeting? ")
+x = raw_input ("What is your greeting? ")
+
+print "You say: "+x+", "+y+"!"
