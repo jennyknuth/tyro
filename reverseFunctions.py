076dbdd64058fd375b825988af1be159d2e05970
Jenny Knuth <inkboco@gmail.com>
2015-02-21 18:10:03 -0700

    making it compatible with Python 2

--- reverseFunctions.py
+++ reverseFunctions.py
@@ -2,14 +2,14 @@
 #Reverse a List or a String using concatenation (my favorite method so far)
 
 text="My country 'tis of thee!"
-print ("Here is some text: ", text)
+print ("Here is some text: %s" % text)
 
 textList = text.split(" ")#turn string into a List
-print ("Text as list: ", textList)
+print ("Text as list: %s" % textList)
 
 myList = [1,12,24,"sing",42,56,73,89,99,201,101]
 l = myList[:] #make a copy of original list
-print ("List of stuff: ", l)
+print ("List of stuff: %s" % l)
 
 #reverseList
 #Takes a list and reverses it
@@ -31,15 +31,13 @@ def reverseString(myString):
 	return (newString)
 
 l = reverseList(l)
-print ("Reverse list: ", l)
+print ("Reverse list: %s" % l)
 
 newTextList = reverseList(textList)
-print ("Reverse text as a list: ", newTextList)
+print ("Reverse text as a list: %s" % newTextList)
 
 newText=" ".join(newTextList)
-print ("Reverse text as a string: ", newText)
+print ("Reverse text as a string: %s" % newText)
 
 reverseText = reverseString(text)
-print ("Reverse entire text string: ", reverseText)
-
-#test
+print("Reverse entire text string: %s" % reverseText)
